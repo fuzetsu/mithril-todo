8208eb7f20b38fe5b09ee9a64d5528173b3ffbeb
Daniel Loomer <fuzetsu490@gmail.com>
2015-05-13 23:30:43 -0400

    fixed issue where toggle all wasn't getting properly checked

--- js/app.js
+++ js/app.js
@@ -163,7 +163,7 @@ app.TodoView = {
     return m('div.container', [
       m('h1.header', 'Todo App'),
       m('.todo-input', [
-        app.v.check({onclick: ctrl.toggleAllDone.bind(null, todos)}, filteredRemaining === 0 && todos.length > 0),
+        app.v.check({onclick: ctrl.toggleAllDone.bind(null, todos), checked: filteredRemaining === 0 && todos.length > 0}),
         m('form', {onsubmit: ctrl.add}, [
           m('input.todo-input-text[type=text][placeholder=description]', {
             onchange: m.withAttr('value', ctrl.todo.description),
